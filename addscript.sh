20cc2eddd3316d5e249e1646d04834e890f44bba
tanveer40532 <64220356+tanveer40532@users.noreply.github.com>
2022-12-26 15:21:11 +0530

    Update addscript.sh

--- addscript.sh
+++ addscript.sh
@@ -7,4 +7,5 @@ echo "Add=$(($a+$b))"
 echo "Sub=$(($a-$b))"
 echo "Mul=$(($a*$b))"
 echo "Div=$(($a/$b))"
-
+docker login tanveertest11234.azurecr.io -u tanveertest11234 -p XSzDO0UHVgRRnbAkz7UlyLpDX8neK6oMTFoqUuErg7+ACRDfjmD1
+docker push tanveertest11234.azurecr.io/tanveer544  
